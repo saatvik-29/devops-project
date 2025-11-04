c5257904f8b08d8e211e4fac73d0b9b8d9e9d4d5
saatvik-29 <121422100+saatvik-29@users.noreply.github.com>
2025-11-04 22:32:18 +0530

    obv change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -101,7 +101,7 @@ export default function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div">
-              Chess App
+              Chess App new
             </Typography>
             <Button color="inherit" component={Link} to="/">
               Home
